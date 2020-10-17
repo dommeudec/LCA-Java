ceea25d7028d0847ed64588df0cce358a4899084
dommeudec <72738163+dommeudec@users.noreply.github.com>
2020-10-17 14:38:43 +0100

    Update LowestCommonAncestry.java
    Updated LCA, main has been removed due to JUnit testing being used

--- LowestCommonAncestry.java
+++ LowestCommonAncestry.java
@@ -75,29 +75,4 @@ public class LowestCommonAncestry {
   
         return false; 
     } 
-  
-    // Driver code 
-    public static void main(String[] args) 
-    { 
-    	LowestCommonAncestry tree = new LowestCommonAncestry(); 
-        tree.root = new Node(1); 
-        tree.root.left = new Node(2); 
-        tree.root.right = new Node(3); 
-        tree.root.left.left = new Node(4); 
-        tree.root.left.right = new Node(5); 
-        tree.root.right.left = new Node(6); 
-        tree.root.right.right = new Node(7); 
-        tree.root.left.left.left = new Node(8);
-        tree.root.left.left.left.left = new Node(9);
-        tree.root.left.left.left.right = new Node(10);
-  
-        System.out.println("LCA(4, 5): " + tree.findLCA(4,5)); 
-        System.out.println("LCA(4, 6): " + tree.findLCA(4,6)); 
-        System.out.println("LCA(3, 4): " + tree.findLCA(3,4)); 
-        System.out.println("LCA(2, 4): " + tree.findLCA(2,4)); 
-        System.out.println("LCA(9, 10): " + tree.findLCA(9,10));
-        System.out.println("LCA(5, 10): " + tree.findLCA(5,10));
-        System.out.println("LCA(2, 11): " + tree.findLCA(2,11));
-      
-    } 
 } 
